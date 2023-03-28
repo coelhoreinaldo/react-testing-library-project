8827459a0d13fc46a01ae13739e83cb9a9acc305
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-28 15:25:40 -0300

    test: req 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,7 +1,5 @@
-import { screen, act } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
 import React from 'react';
-import App from '../App';
 import { About } from '../pages';
 import renderWithRouter from '../renderWithRouter';
 
@@ -14,11 +12,19 @@ describe('About.js', () => {
     });
     expect(pokedexTitle).toBeInTheDocument();
   });
+
   it('should contain pokedex info', () => {
     renderWithRouter(<About />);
     const aboutPokedex1 = screen.getByText(/This application simulates a Pokédex, a digital encyclopedia containing all Pokémon/i);
     const aboutPokedex2 = screen.getByText(/One can filter Pokémon by type, and see more details for each one of them/i);
     expect(aboutPokedex1).toBeInTheDocument();
     expect(aboutPokedex2).toBeInTheDocument();
+  });
+
+  it('should contain a pokedex image', () => {
+    renderWithRouter(<About />);
+    const imagePokedex = screen.getByRole('img', { name: /pokédex/i });
+    expect(imagePokedex).toBeInTheDocument();
+    expect(imagePokedex.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
