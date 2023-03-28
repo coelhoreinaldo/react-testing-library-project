7d7fc32412828dc4b03e90e9d5943c9fc98bac60
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-28 15:00:49 -0300

    test: Req 1.4

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
@@ -11,7 +12,37 @@ describe('App.js', () => {
     expect(homeLink).toBeInTheDocument();
     const aboutLink = screen.getByRole('link', { name: /about/i });
     expect(aboutLink).toBeInTheDocument();
-    const favoriteLink = screen.getByRole('link', { name: /favorite/i });
-    expect(favoriteLink).toBeInTheDocument();
+    const favoritesLink = screen.getByRole('link', { name: /favorite/i });
+    expect(favoritesLink).toBeInTheDocument();
+  });
+
+  it('should redirect to / on home link click', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    userEvent.click(homeLink);
+    const { location: { pathname } } = history;
+
+    expect(pathname).toBe('/');
+  });
+
+  it('should redirect to /about on about link click', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    userEvent.click(aboutLink);
+    const { location: { pathname } } = history;
+
+    expect(pathname).toBe('/about');
+  });
+
+  it('should redirect to /favorites on favorite link click', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favoritesLink = screen.getByRole('link', { name: /favorite/i });
+    userEvent.click(favoritesLink);
+    const { location: { pathname } } = history;
+
+    expect(pathname).toBe('/favorites');
   });
 });
