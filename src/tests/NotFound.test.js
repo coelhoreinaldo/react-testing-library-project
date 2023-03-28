c09347075f448795cc5b257dd37b48df6fc19409
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-28 15:48:57 -0300

    test: req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('NotFound.js', () => {
+  it('should have a h2 with "Page requested not found" text', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundText = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  it('should have a specific image', () => {
+    renderWithRouter(<NotFound />);
+
+    const pikachuCryingImg = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(pikachuCryingImg).toBeInTheDocument();
+    expect(pikachuCryingImg.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
