c199866ea4fb09608811e2245eb121d708287ae9
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-28 16:57:57 -0300

    test: req 5.3

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -3,61 +3,61 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import { Pokedex } from '../pages';
-// import pokemonList from '../data';
-
-const isPokemonFavorite = {
-  4: true,
-  10: false,
-  25: true,
-};
+import pokemonList from '../data';
 
 // const isPokemonFavorite = {
 //   4: true,
 //   10: false,
-//   23: false,
 //   25: true,
-//   65: false,
-//   78: false,
-//   143: false,
-//   148: false,
-//   151: false,
 // };
 
-const pokemonList = [
-  {
-    id: 25,
-    name: 'Pikachu',
-    type: 'Electric',
-    averageWeight: {
-      value: '6.0',
-      measurementUnit: 'kg',
-    },
-    image: 'https://archives.bulbagarden.net/media/upload/b/b2/Spr_5b_025_m.png',
-    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
-  },
-  {
-    id: 4,
-    name: 'Charmander',
-    type: 'Fire',
-    averageWeight: {
-      value: '8.5',
-      measurementUnit: 'kg',
-    },
-    image: 'https://archives.bulbagarden.net/media/upload/0/0a/Spr_5b_004.png',
-    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
-  },
-  {
-    id: 10,
-    name: 'Caterpie',
-    type: 'Bug',
-    averageWeight: {
-      value: '2.9',
-      measurementUnit: 'kg',
-    },
-    image: 'https://archives.bulbagarden.net/media/upload/8/83/Spr_5b_010.png',
-    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)',
-  },
-];
+const isPokemonFavorite = {
+  4: true,
+  10: false,
+  23: false,
+  25: true,
+  65: false,
+  78: false,
+  143: false,
+  148: false,
+  151: false,
+};
+
+// const pokemonList = [
+//   {
+//     id: 25,
+//     name: 'Pikachu',
+//     type: 'Electric',
+//     averageWeight: {
+//       value: '6.0',
+//       measurementUnit: 'kg',
+//     },
+//     image: 'https://archives.bulbagarden.net/media/upload/b/b2/Spr_5b_025_m.png',
+//     moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+//   },
+//   {
+//     id: 4,
+//     name: 'Charmander',
+//     type: 'Fire',
+//     averageWeight: {
+//       value: '8.5',
+//       measurementUnit: 'kg',
+//     },
+//     image: 'https://archives.bulbagarden.net/media/upload/0/0a/Spr_5b_004.png',
+//     moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+//   },
+//   {
+//     id: 10,
+//     name: 'Caterpie',
+//     type: 'Bug',
+//     averageWeight: {
+//       value: '2.9',
+//       measurementUnit: 'kg',
+//     },
+//     image: 'https://archives.bulbagarden.net/media/upload/8/83/Spr_5b_010.png',
+//     moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)',
+//   },
+// ];
 
 describe('Pokedex.js', () => {
   it('should have an h2 with the text "Encountered Pokémon"', () => {
@@ -87,10 +87,26 @@ describe('Pokedex.js', () => {
 
     const caterpie = screen.getByText(/caterpie/i);
     expect(caterpie).toBeInTheDocument();
+  });
 
-    userEvent.click(nextBtn);
+  it('should show 8 filter buttons', () => {
+    renderWithRouter(<Pokedex
+      pokemonList={ pokemonList }
+      isPokemonFavoriteById={ isPokemonFavorite }
+    />);
 
-    const pikachu1 = screen.getByText(/pikachu/i);
-    expect(pikachu1).toBeVisible();
+    const buttons = screen.getAllByTestId('pokemon-type-button');
+    const buttonTypes = pokemonList.map(({ type }) => type);
+    expect(buttons).toHaveLength(7);
+    buttons.forEach((button) => expect(buttonTypes).toContain(button.innerHTML));
+
+    userEvent.click(buttons[1]);
+    const pikachu = screen.queryByText(/pikachu/i);
+    expect(pikachu).not.toBeInTheDocument();
+    const charmander = screen.getByText(/charmander/i);
+    expect(charmander).toBeInTheDocument();
+
+    const allButton = screen.getByRole('button', { name: 'All' });
+    expect(allButton).toBeVisible();
   });
 });
