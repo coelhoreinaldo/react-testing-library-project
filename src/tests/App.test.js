ab5267ade073ed48225556ba468c289985fd4b19
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-28 14:51:03 -0300

    test: Req 1.1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('App.js', () => {
+  it('should have three links on the header', () => {
+    renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    expect(homeLink).toBeInTheDocument();
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    expect(aboutLink).toBeInTheDocument();
+    const favoriteLink = screen.getByRole('link', { name: /favorite/i });
+    expect(favoriteLink).toBeInTheDocument();
+  });
+});
